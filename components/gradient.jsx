0f725fe9fc11b4ea5c04777f2689fd9b4f2da330
Cristian Bilu <bilucristian25@gmail.com>
2022-03-10 21:47:55 +0200

    Change hours for the gradient

--- components/gradient.jsx
+++ components/gradient.jsx
@@ -19,12 +19,12 @@ const Gradient = () => {
     setDelay((60 - minutes) * 60 * 1000);
   }, [minutes]);
 
-  if (hour > 11 && hour < 18) {
+  if (hour > 11 && hour < 17) {
     return (
       <div className="absolute inset-0 bg-gradient-to-b from-sky-700 to-sky-200"></div>
     );
   }
-  if ((hour >= 0 && hour < 7) || (hour > 21 && hour < 24)) {
+  if ((hour >= 0 && hour < 7) || (hour > 20 && hour < 24)) {
     return (
       <div className="absolute inset-0 bg-gradient-to-b from-sky-900 to-gray-900"></div>
     );
