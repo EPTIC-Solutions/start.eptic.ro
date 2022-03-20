99b347786d536275339cada9aa9b33772173b489
Cristian Bilu <bilucristian25@gmail.com>
2022-03-20 22:01:59 +0200

    Add better key handling logic

--- components/Engines.jsx
+++ components/Engines.jsx
@@ -1,85 +1,36 @@
 import { useEngine } from "../utils/useEngine";
 import { useEffect } from "react";
 import styles from "../styles/Home.module.css";
+// import useOS from "../utils/useOS";
 
 const Engines = () => {
   const { engines, isActive, setActive, setClearInput } = useEngine();
-
-  const getOperatingSystem = () => {
-    let operatingSystem = "Not known";
-    if (
-      typeof navigator.userAgentData === "undefined" ||
-      typeof navigator.userAgentData.platform === "undefined"
-    ) {
-      if (navigator.platform.indexOf("Win") !== -1) {
-        operatingSystem = "Windows OS";
-      }
-      if (navigator.platform.indexOf("Mac") !== -1) {
-        operatingSystem = "MacOS";
-      }
-      if (navigator.platform.indexOf("X11") !== -1) {
-        operatingSystem = "UNIX OS";
-      }
-      if (navigator.platform.indexOf("Linux") !== -1) {
-        operatingSystem = "Linux OS";
-      }
-      return operatingSystem;
-    }
-
-    if (navigator.userAgentData.platform.indexOf("Win") !== -1) {
-      operatingSystem = "Windows OS";
-    }
-    if (navigator.userAgentData.platform.indexOf("Mac") !== -1) {
-      operatingSystem = "MacOS";
-    }
-    if (navigator.userAgentData.platform.indexOf("X11") !== -1) {
-      operatingSystem = "UNIX OS";
-    }
-    if (navigator.userAgentData.platform.indexOf("Linux") !== -1) {
-      operatingSystem = "Linux OS";
-    }
-    return operatingSystem;
-  };
+  // const operatingSystem = useOS();
 
   const getControlKey = () => {
-    // if (getOperatingSystem() === "Windows OS") return "Alt";
+    // if (operatingSystem === "Windows") return "Alt";
     return "Alt";
   };
 
   useEffect(() => {
-    let checkInput = false;
-
     const handleKeyDown = (event) => {
-      // Control
-      if (event.key === getControlKey()) {
-        checkInput = true;
-      }
-      if (checkInput) {
-        try {
-          const keyInt = parseInt(event.key);
-          if (keyInt > 0 && keyInt <= 9) {
-            setActive(keyInt - 1);
-          } else if (event.key === "c") {
-            setClearInput(true);
-          } else {
-            return;
-          }
-          event.preventDefault();
-        } catch {}
-      }
-    };
-
-    const handleKeyUp = (event) => {
-      if (event.key === getControlKey()) {
-        checkInput = false;
-      }
+      try {
+        if (!event.altKey) return;
+        const keyInt = parseInt(event.key);
+        if (keyInt > 0 && keyInt <= 9) {
+          setActive(keyInt - 1);
+        } else if (event.key === "c") {
+          setClearInput(true);
+        } else {
+          return;
+        }
+        event.preventDefault();
+      } catch {}
     };
 
     window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
     };
   }, []);
 
