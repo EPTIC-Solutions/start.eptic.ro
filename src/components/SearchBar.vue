874139cffd716b429742cf2e12e9afc76e76bbf1
Cristian Bilu <cristian@eptic.ro>
2024-11-24 00:48:57 +0200

    Always focus search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,12 +2,17 @@
 import { useEngineStore } from '@/stores/engine'
 import isMac from '@/utils/isMac';
 import { storeToRefs } from 'pinia'
-import { onMounted, onUnmounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref, watch } from 'vue'
 
 const engineStore = useEngineStore()
 const { currentEngine, engines } = storeToRefs(engineStore)
 
 const search = ref('')
+const inputEl = ref<HTMLInputElement>()
+
+watch(currentEngine, () => {
+  inputEl.value?.focus()
+})
 
 const onKeyDown = (event: KeyboardEvent) => {
   if ((isMac() && !event.metaKey) || (!isMac() && !event.ctrlKey)) return
@@ -33,8 +38,8 @@ onUnmounted(() => {
     }
       ">
       <div id="input">
-        <input autofocus="true" type="text" v-model="search" :placeholder="`Search ${engines[currentEngine].name}`"
-          autocomplete="off" class="text-4xl sm:text-6xl" />
+        <input autofocus="true" type="text" ref="inputEl" v-model="search"
+          :placeholder="`Search ${engines[currentEngine].name}`" autocomplete="off" class="text-4xl sm:text-6xl" />
       </div>
     </form>
   </div>
