5010c7eb1d7d1a12112d2ea46926e73ac40e0673
Cristian Bilu <cristian@eptic.ro>
2025-04-02 23:51:56 +0300

    WIP

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{vue,ts}'],
-  theme: {
-    extend: {}
-  },
-  plugins: [require('@tailwindcss/typography')]
-}
